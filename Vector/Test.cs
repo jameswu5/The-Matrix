61f83a345ddcf458c84c64d0fd6b284cc316f1bf
James Wu <jameswu144@gmail.com>
2023-11-22 15:15:11 +0000

    add vector implementation

---
+++ Vector/Test.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearAlgebra.Vector;
+
+public static class Test {
+    private static Vector vec1 = new Vector(new double[] {1.5, 4.5, 5.5});
+    private static Vector vec2 = new Vector(new int[] {1, 4, 5});
+    private static Vector vec3 = new Vector(3);
+
+    public static void Playground() {
+
+    }
+}
---
+++ Vector/Vector.cs
@@ -0,0 +1,66 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LinearAlgebra.Vector;
+
+public partial class Vector {
+
+    public int Length { get; private set; }
+    private readonly double[] vector;
+    private const int MaxDecimals = 3;
+
+    /// <summary>
+    /// Create empty vector with all values set to 0.
+    /// </summary>
+    /// <param name="m">Number of elements in the vector</param>
+    public Vector(int m) {
+        vector = new double[m];
+        Length = m;
+    }
+
+    /// <summary>
+    /// Create vector with predefined values.
+    /// </summary>
+    public Vector(double[] vector) {
+        this.vector = vector;
+        Length = vector.Length;
+    }
+
+    /// <summary>
+    /// Create vector with predefined values.
+    /// </summary>
+    public Vector(int[] vector) {
+        Length = vector.Length;
+        this.vector = new double[Length];
+        for (int i = 0; i < Length; i++) {
+            this.vector[i] = vector[i];
+        }
+    }
+
+
+    public override string ToString() {
+        StringBuilder sb = new();
+        foreach (double d in vector) {
+            sb.Append(d);
+            sb.Append(' ');
+        }
+        return sb.ToString();
+    }
+
+    public double this[int index] {
+        get {
+            if (index < 0 || index >= Length) {
+                throw new Exception("Index out of range");
+            }
+            return vector[index];
+        }
+        set {
+            if (index < 0 || index >= Length) {
+                throw new Exception("Index out of range");
+            }
+            vector[index] = value;
+        }
+    }
+
+}
