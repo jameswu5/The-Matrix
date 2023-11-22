355164154180e4e62bbe7795b4afb4b6f2d65959
James Wu <jameswu144@gmail.com>
2023-11-22 19:02:14 +0000

    implement cross product

--- Vector/Product.cs
+++ Vector/Product.cs
@@ -1,5 +1,6 @@
 using System;
 using System.Collections.Generic;
+using System.Reflection.PortableExecutable;
 using System.Text;
 
 namespace LinearAlgebra.Vector;
@@ -22,4 +23,24 @@ public partial class Vector {
     }
 
     public double Dot(Vector vec) => Dot(this, vec);
+
+
+    /// <summary>
+    /// Perform the cross product on two dimension 3 vectors.
+    /// </summary>
+    public static Vector Cross(Vector vec1, Vector vec2) {
+        if (vec1.Length != 3 || vec2.Length != 3) {
+            throw new Exception("Vector sizes must both be 3");
+        }
+
+        Vector res = new Vector(3);
+
+        res[0] = vec1[1] * vec2[2] - vec1[2] * vec2[1];
+        res[1] = vec1[2] * vec2[0] - vec1[0] * vec2[2];
+        res[2] = vec1[0] * vec2[1] - vec1[1] * vec2[0];
+
+        return res;
+    }
+
+    public Vector Cross(Vector vec) => Cross(this, vec);
 }
